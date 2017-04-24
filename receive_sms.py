a53e48144c9b77b625dbf66a7a02ba3384bdb401
Mica Swyers <micaswyers@gmail.com>
2017-04-23 21:45:09 -0700

    Initial commit of receive_sms file

---
+++ receive_sms.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import (
+	Flask,
+	request,
+	redirect,
+)
+from twilio.twiml.messaging_response import MessagingResponse
+
+from eightball import PHRASES, EightBall
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def respond_sms():
+	"""Respond to incoming texts"""
+
+	e = EightBall()
+	resp = MessagingResponse()
+	resp.message(e.shake())
+
+	return str(resp)
+
+if __name__ == "__main__":
+	app.run(debug=True)
